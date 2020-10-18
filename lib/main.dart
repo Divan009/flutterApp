fdc4c164211b0b6a4b986725dbf1c314128d9667
Diva Dugar <divadugar@gmail.com>
2020-10-18 20:06:21 +0530

    ch1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,24 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+//import 'package:flutter/cupertino.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hello World Demo Application',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Home page'),
-    );
-  }
+void main() {
+  runApp(MaterialApp(
+    title: "Hello App", //minimize the app then it shows
+    home: HomePage(),
+  ));
 }
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
 
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(this.title),
-      ),
-      body: Center(
-          child:
-          Text(
-            'Hello World',
-          )
+        title: Text("Hello Flutter"), //shown in app
+      ), //is head
+      body: Container(
+        child: Center(child: Text("Hi Flutter App")),
       ),
     );
   }
