8b77cfe4b9a95e53045183ecb09825e6633af9c9
Diva Dugar <divadugar@gmail.com>
2020-10-18 20:42:12 +0530

    ch2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,37 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Hello Flutter"), //shown in app
-      ), //is head
-      body: Container(
-        child: Center(child: Text("Hi Flutter App")),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Hello Flutter"), //shown in app
+        ), //is head
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            alignment: Alignment.center,
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+                // shape: BoxShape.circle,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey[800],
+                      blurRadius: 5,
+                      offset: Offset(2.0, 5.0))
+                ],
+                color: Colors.tealAccent,
+                gradient:
+                    LinearGradient(colors: [Colors.yellow, Colors.pinkAccent])),
+            child: Text(
+              "I am a box",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ));
   }
 }
